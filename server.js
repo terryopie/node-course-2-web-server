4c9fb1e448034640eea283077e59282bbcd12202
Terry J. Opie <terry.opie@opiescomputers.com>
2017-10-06 10:44:18 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname +'/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) =>{
+        if(err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next(); // Allows Express to move on.
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: 'Maintenance Mode'
+//     })
+// });
+
+// Moved down here to ensure that maintenance mode is honored for
+// static paths.
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to our Site!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'Error handling request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000.');
+});
